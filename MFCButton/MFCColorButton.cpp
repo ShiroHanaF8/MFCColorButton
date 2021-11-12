62b72f0657fe2bc58f2914fd1683b2fe45bf8cf8
ShiroHanaF8 <53520538+ShiroHanaF8@users.noreply.github.com>
2021-11-12 23:30:42 +0900

    起動中にハイコントラストへの切り替えに対応

--- MFCButton/MFCColorButton.cpp
+++ MFCButton/MFCColorButton.cpp
@@ -3,28 +3,16 @@
 
 
 MFCColorButton::MFCColorButton()
-	:m_uncheckedBrush(GetGlobalData()->clrBtnLight),
-	m_borderPen(PS_SOLID, 0, GetGlobalData()->clrActiveBorder)
 {
 	m_bDontUseWinXPTheme = FALSE;
 	m_nFlatStyle = BUTTONSTYLE_3D;
-
-
-
-	if (GetGlobalData()->IsHighContrastMode()) {
-		m_checkedBrush.CreateSolidBrush(GetGlobalData()->clrBtnShadow);
-	}
-	else {
-		m_checkedBrush.CreateSolidBrush(RGB(210, 240, 255));
-	}
 
 	SetButtonStyleFromBuildVersion();
 }
 
 void MFCColorButton::SetButtonStyleFromBuildVersion()
 {
-	HMODULE hMod;
-	hMod = LoadLibrary(_T("ntdll.dll"));
+	HMODULE hMod = GetModuleHandle(_T("ntdll.dll")); // ロード済みのため再ロードしない
 	if (hMod) {
 		OSVERSIONINFOEXW osw = {};
 		void(WINAPI * func)(OSVERSIONINFOEXW*) = (void(WINAPI*)(OSVERSIONINFOEXW*))GetProcAddress(hMod, "RtlGetVersion");
@@ -55,66 +43,43 @@ void MFCColorButton::SetButtonStyleFromBuildVersion()
 			m_isRoundButton = false;
 		}
 	}
+	else {
+		m_isRoundButton = false;
+	}
 }
 
 MFCColorButton::~MFCColorButton()
 {
-	m_checkedBrush.DeleteObject();
-	m_uncheckedBrush.DeleteObject();
 }
-
-
-BOOL MFCColorButton::OnWndMsg(UINT message, WPARAM wParam, LPARAM lParam, LRESULT* pResult)
-{
-	return CMFCButton::OnWndMsg(message, wParam, lParam, pResult);
-}
-
-//void MFCColorButton::OnFillBackground(CDC* pDC, const CRect& rectClient)
-//{
-//	if (IsChecked()) {
-//		m_clrFace = RGB(255, 0, 0); // 赤
-//	}
-//	else {
-//		m_clrFace = RGB(0, 0, 255); // 青
-//	}
-//	return CMFCButton::OnFillBackground(pDC, rectClient);
-//
-//}
 
 void MFCColorButton::OnDrawBorder(CDC* pDC, CRect& rectClient, UINT uiState)
 {
 	CMFCButton::OnDrawBorder(pDC, rectClient, uiState);
 
-	CBrush *newBrush = nullptr;
-
-	if (m_bHighlighted && !IsChecked()) {
-		return;
+	if (IsChecked()) {
+		COLORREF color = {};
+		if (IsHighContrastMode()) {
+			color = GetGlobalData()->clrBtnShadow;
+		}
+		else {
+			color = m_checkedColor;
+		}
+		CBrush brush(color);
+		auto oldBrush = SelectObject(pDC->m_hDC, brush);
+		CPen pen(PS_SOLID, 0, GetGlobalData()->clrActiveBorder);
+		auto oldPen = SelectObject(pDC->m_hDC, pen);
+		const int roundNum = (m_isRoundButton && !IsHighContrastMode()) ? m_roundSize : 0;
+		POINT round = { roundNum, roundNum };
+		pDC->RoundRect(rectClient, round);
+		SelectObject(pDC->m_hDC, oldBrush);
+		SelectObject(pDC->m_hDC, oldPen);
 	}
-	else {
-		newBrush = IsChecked() ? &m_checkedBrush : nullptr;
-	}
-
-	if (newBrush == nullptr) {
-		return;
-	}
-
-	auto oldBrush = SelectObject(pDC->m_hDC, *newBrush);
-	auto oldPen = SelectObject(pDC->m_hDC, m_borderPen);
-	const int roundNum = (m_isRoundButton && !GetGlobalData()->IsHighContrastMode()) ? m_roundSize : 0;
-	POINT round = { roundNum, roundNum };
-	pDC->RoundRect(rectClient, round);
-	SelectObject(pDC->m_hDC, oldBrush);
-	SelectObject(pDC->m_hDC, oldPen);
-
 }
 
-//void MFCColorButton::OnDraw(CDC* pDC, const CRect& rect, UINT uiState)
-//{
-//
-//	return CMFCButton::OnDraw(pDC, rect, uiState);
-//}
-
-void MFCColorButton::OnDrawFocusRect(CDC* pDC, const CRect& rectClient)
+BOOL MFCColorButton::IsHighContrastMode()
 {
-	return CMFCButton::OnDrawFocusRect(pDC, rectClient);
+	HIGHCONTRAST hc = {};
+	hc.cbSize = sizeof(HIGHCONTRAST);
+	SystemParametersInfo(SPI_GETHIGHCONTRAST, sizeof(HIGHCONTRAST), &hc, 0);
+	return hc.dwFlags & HCF_HIGHCONTRASTON;
 }
--- MFCButton/MFCColorButton.h
+++ MFCButton/MFCColorButton.h
@@ -8,21 +8,16 @@ public:
     virtual ~MFCColorButton();
 
 private:
-    CBrush m_checkedBrush;
-    CBrush m_uncheckedBrush;
-    CPen m_borderPen;
     bool m_isRoundButton = false;
     int m_roundSize = 5; // Windows11ÇÃïWèÄ
 
     void SetButtonStyleFromBuildVersion();
-public:
-    virtual BOOL OnWndMsg(UINT message, WPARAM wParam, LPARAM lParam, LRESULT* pResult);
+
+    const COLORREF m_checkedColor = RGB(210, 240, 255);
 
 protected:
-    //virtual void OnFillBackground(CDC* pDC, const CRect& rectClient);
 
     virtual void OnDrawBorder(CDC* pDC, CRect& rectClient, UINT uiState);
-    //virtual void OnDraw(CDC* pDC, const CRect& rect, UINT uiState);
-    virtual void OnDrawFocusRect(CDC* pDC, const CRect& rectClient);
+    BOOL IsHighContrastMode();
 };
 
